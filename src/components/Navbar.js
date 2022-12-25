75493a14c3beed2683b8ffe0c0a4ca2bf05825dc
Adi Hackam <aaaadi01@gmail.com>
2022-12-25 23:58:45 +0200

    Remove warning from navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,34 +7,20 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 import { Link } from "react-router-dom";
 
 
-const pages = ['Products', 'Pricing', 'Blog'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
-
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
   };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
   };
 
   const pages = [
@@ -43,13 +29,13 @@ function ResponsiveAppBar() {
     { name: "Customers", url: "/Customers" },
     { name: "Loans", url: "/Loans" },
   ];
-  const styles = {
-    root: {
-      width: "100%",
-      marginBottom: "10px",
-    },
+  // const styles = {
+  //   root: {
+  //     width: "100%",
+  //     marginBottom: "10px",
+  //   },
 
-  };
+  // };
 
 
   // < Box sx={styles.destopContainer} />
